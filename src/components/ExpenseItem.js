509fd9736fab07e8b0637dca715e699cdc093756
lakshmanK1 <laxmankamisetty1@gmail.com>
2022-10-09 20:54:34 +0530

    Added JS Expressions and used in JSX code, inside parenthesis {..}

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,29 +1,56 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(){
+    const No_Of_Items = {
+        First : 'Item 1',
+        Second : 'Item 2',
+        Third : 'Item 3'
+    }
+
+    const items_list = {
+        listOne : 'Food',
+        listTwo : 'Petrol',
+        listThree : 'Movies'
+    }
+    const items_price = {
+        listOnePrice : 'Rs:100/-',
+        listTwoPrice : 'Rs:180/-',
+        listThreePrice : 'Rs:200/-'
+    }
+
+    const LocationOfExpenditure = {
+        locaOne : 'kakinada-AndhraPradesh-India',
+        locaTwo : 'amalapuram-AndhraPradesh-India',
+        locaThree : 'rajamundry-AndhraPradesh-India'
+    }
+
+
     return (
         <div>
         <div className='expense-item'>
-            <div className='items'>Item 1</div>
+            <div className='items'>{No_Of_Items.First}</div>
             <div className='expense-item__description'>
-                <h2>Food</h2>
-                <div className='expense-item__price'>Rs:150/-</div>
+                <h2>{items_list.listOne}</h2>
+                <h2>{LocationOfExpenditure.locaOne}</h2>
+                <div className='expense-item__price'>{items_price.listOnePrice}</div>
             </div>
         </div>
 
         <div className='expense-item'>
-            <div className='items'>Item 2</div>
+            <div className='items'>{No_Of_Items.Second}</div>
             <div className='expense-item__description'>
-                <h2>Petrol</h2>
-                <div className='expense-item__price'>Rs:200/-</div>
+                <h2>{items_list.listTwo}</h2>
+                <h2>{LocationOfExpenditure.locaTwo}</h2>
+                <div className='expense-item__price'>{items_price.listTwoPrice}</div>
             </div>
         </div>
 
         <div className='expense-item'>
-            <div className='items'>Item 3</div>
+            <div className='items'>{No_Of_Items.Third}</div>
             <div className='expense-item__description'>
-                <h2>Movie</h2>
-                <div className='expense-item__price'>Rs:400/-</div>
+                <h2>{items_list.listThree}</h2>
+                <h2>{LocationOfExpenditure.locaThree}</h2>
+                <div className='expense-item__price'>{items_price.listThreePrice}</div>
             </div>
         </div>
         </div>    
