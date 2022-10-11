1c434fb943b423a6a39e241a63576f7713d4ed7f
lakshmanK1 <laxmankamisetty1@gmail.com>
2022-10-11 20:55:39 +0530

    added buttons for updateTitle and updateCost with functionality

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,16 +1,33 @@
+import React,{ useState } from 'react';
+
 import './ExpenseItem.css';
 import Card from '../UI/Card';
 
 const ExpenseDetails = (props) => {
-
+    // To Change Tittle
+    const [titleData, setTitle] = useState(props.ExpenseType);
+    const UpdateTitle = () => {
+        setTitle("Updating..");
+        console.log(titleData);
+    }
+    
+    // To Change Cost
+    const [costData, setCost] = useState(props.ExpenseCost);
+    const UpdateCost = () => {
+        setCost('100$');
+        console.log(costData);
+    }
+ 
     return (
     <Card className='expense-item'>
         <div className='items'>{props.ExpenseCount}</div>
             <div className='expense-item__description'>
-                <h2>{props.ExpenseType}</h2>
+                <h2>{titleData}</h2>
                 <h2>{props.ExpenseLocation}</h2>
-                <div className='expense-item__price'>{props.ExpenseCost}</div>
+                <div className='expense-item__price'>{costData}</div>
             </div>
+            <button onClick={UpdateTitle}>Title</button>
+            <button onClick={UpdateCost}>Cost</button>
     </Card>
     );
 }
