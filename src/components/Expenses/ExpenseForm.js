8efbfa86dd4e4b7ffa72a4b8e3570cc3e66ecd27
lakshmanK1 <laxmankamisetty1@gmail.com>
2022-10-12 14:49:17 +0530

    added functionality to update(in console) Title, Amount, Date when we submit FORM

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -1,24 +1,53 @@
+import React,{useState} from 'react';
+
 import './ExpenseForm.css';
 function ExpenseForm(){
-    const consoleForm = (event) => {
-        console.log(event.target.value);
+
+    const [TitleVal, setTitleVal] = useState('');
+    const [AmountVal, setAmountVal] = useState('');
+    const [DateVal, setDateVal] = useState('');
+
+
+    const updateTitle = (event) => {
+        setTitleVal(event.target.value);
+    }
+
+    const updateAmount = (event) => {
+        setAmountVal(event.target.value);
+    }
+
+    const updateDate = (event) => {
+        setDateVal(event.target.value);
+    }
+
+    const submitData = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title : TitleVal,
+            amount : AmountVal,
+            date : new Date(DateVal)
+        }
+
+        console.log(expenseData);
+
     } 
 
     return(
         <div className="new-expense">
-        <form onChange={consoleForm}>
+        <form onSubmit={submitData}>
             <div className="new-expense__controls">
                 <div className="new-expense__controls">
                     <label>Title</label>
-                    <input type='text'></input>
+                    <input type='text' onChange={updateTitle}></input>
                 </div>
                 <div className="new-expense__controls">
                     <label>Amount</label>
-                    <input type='number'></input>
+                    <input type='number' onChange={updateAmount}></input>
                 </div>
                 <div className="new-expense__controls">
                     <label>Date</label>
-                    <input type='date'></input>
+                    <input type='date' onChange={updateDate}></input>
                 </div>
             </div>
             <div className="new-expense__actions">
