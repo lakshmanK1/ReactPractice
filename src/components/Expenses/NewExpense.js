317b86d7d6e8fa2b46c42f79eab26012b5934d61
lakshmanK1 <laxmankamisetty1@gmail.com>
2022-10-16 18:25:54 +0530

    Added button to open the form when we clicked and also added cancel button inside form

--- src/components/Expenses/NewExpense.js
+++ src/components/Expenses/NewExpense.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
@@ -12,9 +13,20 @@ const NewExpense = (props) => {
     props.onAddExpense(expenseData);
   };
 
+  const [isediting, setIsEditing] = useState(false);
+
+  const startEditing = () => {
+    setIsEditing(true);
+  }
+
+  const stopEditing = () => {
+    setIsEditing(false);
+  }
+
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+     {!isediting && <button onClick={startEditing}>Add Expense Details</button>}
+     {isediting && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} cancelBtn={stopEditing}/>}
     </div>
   );
 };
