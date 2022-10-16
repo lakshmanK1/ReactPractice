804f937e05f4150539bfad39611135ae804263e2
lakshmanK1 <laxmankamisetty1@gmail.com>
2022-10-16 18:26:33 +0530

    Added button to open the form when we clicked and also added cancel button inside form

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -82,6 +82,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className='new-expense__actions'>
+        <button type='button' onClick={props.cancelBtn}>cancel</button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
